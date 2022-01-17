6aa0bfd0b9548e039c10273de5ede593c058ecf1
Massimo Deiana <m.deiana99@gmail.com>
2022-01-17 14:59:25 +0100

    ejb

--- src/main/java/be/helha/aemt/ejb/ActiviteEJB.java
+++ src/main/java/be/helha/aemt/ejb/ActiviteEJB.java
@@ -9,11 +9,35 @@ import javax.transaction.Transactional;
 
 import be.helha.aemt.dao.ActiviteDAO;
 import be.helha.aemt.entities.Activite;
+import be.helha.aemt.entities.Utilisateur;
 
 @Stateless
-@LocalBean
-public class ActiviteEJB extends EntityEJB<Activite> {
+public class ActiviteEJB implements IActiviteRemoteEJB {
 
+	@EJB
+	private ActiviteDAO dao;
+
+	@Override
+	public Activite find(Integer id) {
+		return dao.find(id);
+	}
+
+	@Override
+	public List<Activite> findAll() {
+		return dao.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Activite add(Activite u) {
+		return dao.add(u);
+	}
+
+	@Override
+	@Transactional
+	public Activite delete(Activite u) {
+		return dao.delete(u);
+	}
 
 	
 }
--- src/main/java/be/helha/aemt/ejb/AdresseEJB.java
+++ src/main/java/be/helha/aemt/ejb/AdresseEJB.java
@@ -1,12 +1,39 @@
 package be.helha.aemt.ejb;
 
-import javax.ejb.LocalBean;
-import javax.ejb.Stateless;
+import java.util.List;
 
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.transaction.Transactional;
+
+import be.helha.aemt.dao.AdresseDAO;
 import be.helha.aemt.entities.Adresse;
 
 @Stateless
-@LocalBean
-public class AdresseEJB extends EntityEJB<Adresse> {
+public class AdresseEJB implements IAdresseRemoteEJB {
 
+	@EJB
+	private AdresseDAO dao;
+
+	@Override
+	public Adresse find(Integer id) {
+		return dao.find(id);
+	}
+
+	@Override
+	public List<Adresse> findAll() {
+		return dao.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Adresse add(Adresse u) {
+		return dao.add(u);
+	}
+
+	@Override
+	@Transactional
+	public Adresse delete(Adresse u) {
+		return dao.delete(u);
+	}
 }
